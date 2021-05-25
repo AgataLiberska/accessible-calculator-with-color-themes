c7d9dd5ef84db10356eb449edf2af7ba08413002
Agata Liberska <liberska@gmail.com>
2021-05-25 11:35:58 +0100

    move theme switching function into separate module

--- js/app.js
+++ js/app.js
@@ -1,30 +1,7 @@
+import { setTheme, setInitialTheme } from './themeSwitcher.js';
+
 const inputs = document.querySelectorAll('input');
-const body = document.getElementById('body');
 
-
-function setTheme(theme) {
-    body.classList = theme;
-}
-
-function setInitialTheme() {
-    const darkInput = document.getElementById('dark');
-    const lightInput = document.getElementById('light');
-    const neonInput = document.getElementById('neon');
-    console.log(darkInput);
-
-    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
-
-    if(userPrefersDark){
-        darkInput.setAttribute('checked', '');
-        setTheme('dark-theme');
-    } else if (userPrefersLight) {
-        lightInput.setAttribute('checked', '');
-        setTheme('light-theme');
-    } else {
-        console.log('no-preference');
-    }
-}
 
 setInitialTheme();
 
--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -1,3 +1,24 @@
 function setTheme(theme) {
+    const body = document.getElementById('body');
     body.classList = theme;
 }
+
+function setInitialTheme() {
+    const darkInput = document.getElementById('dark');
+    const lightInput = document.getElementById('light');
+    const neonInput = document.getElementById('neon');
+    console.log(darkInput);
+
+    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+
+    if (userPrefersLight) {
+        lightInput.setAttribute('checked', '');
+        setTheme('light-theme');
+    } else {
+        darkInput.setAttribute('checked', '');
+        setTheme('dark-theme');
+    }
+}
+
+export { setTheme, setInitialTheme };
