5488fd913847d08c07c21962ee0c963062f8810e
Agata Liberska <liberska@gmail.com>
2021-05-27 13:33:29 +0100

    correct input formatting

--- js/functions/calculatorFunctions/calculate.js
+++ js/functions/calculatorFunctions/calculate.js
@@ -1,9 +1,16 @@
 function calculate(input, operators) {
     let result;
+    const divisionByZero = /.*\/0([^.]|$|\.(0{4,}.*|0{1,4}([^0-9]|$))).*/g;
 
     if (operators.includes(input[input.length-1])) {
         input.pop();
     }
+    
+    // if (input.join('').match(divisionByZero)) {
+    //     result = 'Not allowed';
+    // } else {
+        
+    // }
 
     result = new Function('return '+ input.join(''))();
 
--- js/functions/calculatorFunctions/formatOutput.js
+++ js/functions/calculatorFunctions/formatOutput.js
@@ -2,16 +2,21 @@ import findLastOperator from './findLastOperator.js';
 
 function formatOutput(input, operators) {
     const output = [];
+    console.log('input',input);
+    
+    if (!input.length) {
+        return '0';
+    } 
 
     // IF THERE ARE OPERATORS IN THE INPUT, separate the numbers out
-    if (input.some(item => operators.indexOf(item) !== -1)) {
+    else if (input.some(item => operators.indexOf(item) !== -1)) {
         let startSlice = 0;
         let endSlice = 0;
 
         for (let i = 0; i < input.length; i++) {
             if (operators.includes(input[i])) {
                 endSlice = i;
-                let num = Number(input.slice(startSlice, endSlice).join('')).toLocaleString('en-UK');
+                let num = Number(input.slice(startSlice, endSlice).join('')).toLocaleString('en-UK', {maximumFractionDigits: 20});
                 output.push(num);
                 output.push(input[i]);
         
@@ -22,20 +27,24 @@ function formatOutput(input, operators) {
         // don't forget the last number
         if (!operators.includes(input[input.length-1])) {
             const lastOperator = findLastOperator(input, operators);
-            const lastNumber = Number(input.slice(lastOperator + 1).join('')).toLocaleString();
+            const lastNumber = input.slice(lastOperator + 1).join('');
+            console.log('lastnumber', lastNumber);
             output.push(lastNumber);
         }
-    } 
+    } else if (input.includes('Not allowed')) {
+        output.push('Not allowed');
+    }
     //if no operators, just format the number    
     else {
-        output.push(Number(input.join('')).toLocaleString('en-UK'));
+        output.push(input.join(''));
     }
 
-    if (input[input.length-1] === '.') {
-        output.push('.');
-    }
+    // if (input[input.length-1] === '.') {
+    //     output.push('.');
+    // }
 
     // and display
+    console.log(output);
     return output.length > 0 ? output.join('') : '0';
 }
 
