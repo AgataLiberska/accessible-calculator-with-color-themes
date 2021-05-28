e2d66c5f05818dca195fbaac594c315b5516826f
Agata Liberska <liberska@gmail.com>
2021-05-28 14:56:28 +0100

    division by 0 not allowed

--- js/functions/calculatorFunctions/calculate.js
+++ js/functions/calculatorFunctions/calculate.js
@@ -2,17 +2,17 @@ function calculate(input, operators) {
     let result;
     const divisionByZero = /.*\/0([^.]|$|\.(0{4,}.*|0{1,4}([^0-9]|$))).*/g;
 
+    //if the last char is an operator, get rid of it
     if (operators.includes(input[input.length-1])) {
         input.pop();
     }
     
-    // if (input.join('').match(divisionByZero)) {
-    //     result = 'Not allowed';
-    // } else {
-        
-    // }
-
-    result = new Function('return '+ input.join(''))();
+    //we can't divide by zero
+    if (input.join('').match(divisionByZero)) {
+        result = 'Not allowed';
+    } else {
+        result = new Function('return '+ input.join(''))();
+    }    
 
     return result;
 }
